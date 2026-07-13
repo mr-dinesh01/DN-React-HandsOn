2cdd2bfbd0592746eb67073dbdb4ff03421df3bd
mr-dinesh01 <1.dinesh.mr@gmail.com>
2026-07-14 05:02:47 +0530

    Completed React Hands-on 7 - Online Shopping Cart

---
+++ shoppingapp/src/OnlineShopping.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Cart from "./Cart";
+
+class OnlineShopping extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cartItems: [
+                new Cart("Laptop", 65000),
+                new Cart("Mobile", 25000),
+                new Cart("Headphones", 2000),
+                new Cart("Keyboard", 1500),
+                new Cart("Mouse", 800)
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Online Shopping Cart</h1>
+
+                <table border="1" cellPadding="10">
+                    <thead>
+                        <tr>
+                            <th>Item Name</th>
+                            <th>Price (₹)</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.state.cartItems.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.itemName}</td>
+                                <td>{item.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default OnlineShopping;
